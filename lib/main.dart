15fb583ad983f450013a9da4b9d68f7f5c8089be
Oliver <eliel.viana@estudante.ifto.edu.br>
2022-09-19 19:55:35 -0300

    nova funcionalidade

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment direto na DEV',
+        tooltip: 'Increment direto featurePagamento',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
